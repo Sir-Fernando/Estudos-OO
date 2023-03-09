2067e7ad8cb46665e93dc5d25ddb06768aa206c6
Italo Fernando <86246810+Sir-Fernando@users.noreply.github.com>
2023-03-09 07:20:10 -0300

    Add files via upload

--- Data/main.py
+++ Data/main.py
@@ -3,14 +3,14 @@ from Data import Data
 import os
 os.system("cls")
 
+hoje = Data(9, 3, 2023)
+
 dia = int(input('Digite o dia: '))
 mes = int(input('Digite o mês: '))
 ano = int(input('Digite o ano: '))
 
-hoje = Data(dia, mes, ano)
-
-#hoje.dia = dia
-#hoje.mes = mes
-#hoje.ano = ano
+hoje.dia = dia
+hoje.mes = mes
+hoje.ano = ano
 
 print(hoje)
